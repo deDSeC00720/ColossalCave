d5c7facb9e2cd28faf7d0f9efab8911178aab0cb
deDSeC00720 <terminator.karansharma@gmail.com>
2021-09-12 03:27:31 +0530

    use BufferedReader

--- src/com/colossal/Locations.java
+++ src/com/colossal/Locations.java
@@ -9,54 +9,58 @@ public class Locations implements Map<Integer, Location> {
     public static void main(String[] args) throws IOException {
         FileWriter locationsFile = null;
         FileWriter connFile = null;
-//        try {
-//            locationsFile = new FileWriter("locations.txt");
-//            connFile = new FileWriter("connections.txt");
-//            for (Location location : locations.values()) {
-//                locationsFile.write(location.getLocationID() + "," + location.getDesc() + "\n");
-//                for (String connection : location.getConnections().keySet()) {
-//                    connFile.write(location.getLocationID() + "," + connection + "," + location.getConnections().get(connection) + "\n");
-//                }
-//            }
-//        } finally {
-//            if (locationsFile != null) {
-//                locationsFile.close();
-//            }
-//            if (connFile != null) {
-//                connFile.close();
-//            }
-//        }
+        try {
+            locationsFile = new FileWriter("locations.txt");
+            connFile = new FileWriter("connections.txt");
+            for (Location location : locations.values()) {
+                locationsFile.write(location.getLocationID() + "," + location.getDesc() + "\n");
+                for (String connection : location.getConnections().keySet()) {
+                    connFile.write(location.getLocationID() + "," + connection + "," + location.getConnections().get(connection) + "\n");
+                }
+            }
+        } finally {
+            if (locationsFile != null) {
+                locationsFile.close();
+            }
+            if (connFile != null) {
+                connFile.close();
+            }
+        }
     }
 
     static {
-        Scanner scanner = null;
+        BufferedReader locationsFile = null;
         try {
-            scanner = new Scanner(new FileReader("locations_big.txt"));
-            scanner.useDelimiter(",");
-            while (scanner.hasNextLine()) {
-                int locationID = scanner.nextInt();
-                scanner.skip(scanner.delimiter());
-                String desc = scanner.nextLine();
+            locationsFile = new BufferedReader(new FileReader("locations_big.txt"));
+            String line;
+            while ((line = locationsFile.readLine()) != null) {
+                System.out.println(line);
+                String[] data = line.split(",");
+                int locationID = Integer.parseInt(data[0]);
+                String desc = data[1];
                 locations.put(locationID, new Location(locationID, desc));
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (scanner != null) {
-                scanner.close();
+            if (locationsFile != null) {
+                try {
+                    locationsFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        scanner = null;
+        BufferedReader connFile = null;
         try {
-            scanner = new Scanner(new BufferedReader(new FileReader("directions_big.txt")));
-            scanner.useDelimiter(",");
-            while (scanner.hasNextLine()) {
-                int locationID = scanner.nextInt();
-                scanner.skip(scanner.delimiter());
-                String direction = scanner.next();
-                scanner.skip(scanner.delimiter());
-                String connIDString = scanner.nextLine();
-                int connectionID = Integer.parseInt(connIDString);
+            connFile = new BufferedReader(new FileReader("connections_big.txt"));
+            String line;
+            while ((line = connFile.readLine()) != null) {
+                System.out.println(line);
+                String[] data = line.split(",");
+                int locationID = Integer.parseInt(data[0]);
+                String direction = data[1];
+                int connectionID = Integer.parseInt(data[2]);
 
                 locations.get(locationID).addConnection(direction, connectionID);
             }
@@ -64,33 +68,14 @@ public class Locations implements Map<Integer, Location> {
             e.printStackTrace();
         }
         finally {
-            if (scanner != null) {
-                scanner.close();
+            if (connFile != null) {
+                try {
+                    connFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-
-
-//        locations.put(0, new Location(0, "Exit"));
-//        locations.put(1, new Location(1, "You are standing at the end of the road before a small brick building"));
-//        locations.put(2, new Location(2, "You are at the top of the hill"));
-//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-//        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-//        locations.put(5, new Location(5, "You are in the forest"));
-//
-//        locations.get(1).addConnection("W", 2);
-//        locations.get(1).addConnection("E", 3);
-//        locations.get(1).addConnection("S", 4);
-//        locations.get(1).addConnection("N", 5);
-//
-//        locations.get(2).addConnection("N", 5);
-//
-//        locations.get(3).addConnection("W", 1);
-//
-//        locations.get(4).addConnection("N", 1);
-//        locations.get(4).addConnection("W", 2);
-//
-//        locations.get(5).addConnection("S", 1);
-//        locations.get(5).addConnection("W", 2);
     }
 
     @Override
@@ -130,7 +115,7 @@ public class Locations implements Map<Integer, Location> {
 
     @Override
     public void putAll(Map<? extends Integer, ? extends Location> m) {
-
+        locations.putAll(m);
     }
 
     @Override
